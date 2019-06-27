fae063fbe2b79fe1f34ebf9fc898f7a4c1f6a0e4
Dylan Hamada <dylan.hamada@gmail.com>
2019-06-26 21:39:03 -0400

    Add App and Display components to React App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 
-function App() {
-  return <div />;
+function Display(props) {
+  return (
+    <div id="display">
+      <h1 className="text-center">{props.drumPad}</h1>
+    </div>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="drum-machine" className="container bg-primary">
+        <Display drumPad="High Hat" />
+      </div>
+    );
+  }
 }
 
 export default App;
