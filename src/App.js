ecf38f65f584ed56c89cd0553b977c68c41c4c70
Dylan Hamada <dylan.hamada@gmail.com>
2019-06-29 13:53:44 -0400

    Add event handlers and <audio> element to DrumPad component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from "react";
 
 const drumPads = [
-  { keyPress: "Q" },
-  { keyPress: "W" },
-  { keyPress: "E" },
-  { keyPress: "A" },
-  { keyPress: "S" },
-  { keyPress: "D" },
-  { keyPress: "Z" },
-  { keyPress: "X" },
-  { keyPress: "C" }
+  {
+    keyPress: "Q",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "W",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "E",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "A",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "S",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "D",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "Z",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "X",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  },
+  {
+    keyPress: "C",
+    audioSrc: "https://actions.google.com/sounds/v1/cartoon/punchline_drum.ogg"
+  }
 ];
 
 function Display(props) {
@@ -23,12 +50,24 @@ function Display(props) {
 class DrumPad extends React.Component {
   constructor(props) {
     super(props);
+    this.handleDrumPlay = this.handleDrumPlay.bind(this);
+  }
+
+  handleDrumPlay(event) {
+    let audioElement = document.getElementById(event.target.innerHTML);
+    audioElement.play();
+    this.props.onChange(event.target.innerHTML);
   }
 
   render() {
     return (
-      <div className="drum-pad">
+      <div className="drum-pad" onClick={this.handleDrumPlay}>
         <h1>{this.props.letterKey}</h1>
+        <audio
+          id={this.props.letterKey}
+          className="clip"
+          src={this.props.audioSrc}
+        />
       </div>
     );
   }
@@ -37,16 +76,31 @@ class DrumPad extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      drumName: "High Hat"
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(drumName) {
+    this.setState({
+      drumName: drumName
+    });
   }
 
   render() {
     const drumList = drumPads.map(drumPad => (
-      <DrumPad key={drumPad.keyPress} letterKey={drumPad.keyPress} />
+      <DrumPad
+        key={drumPad.keyPress}
+        letterKey={drumPad.keyPress}
+        audioSrc={drumPad.audioSrc}
+        onChange={this.handleChange}
+      />
     ));
 
     return (
       <div id="drum-machine" className="container bg-primary">
-        <Display drumPad="High Hat" />
+        <Display drumPad={this.state.drumName} />
         {drumList}
       </div>
     );
