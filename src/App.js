e837c3d595bb0293c84cb0f638a9b7bf104dc971
Dylan Hamada <dylan.hamada@gmail.com>
2019-06-28 22:03:10 -0400

    Add DrumPad components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+const drumPads = [
+  { keyPress: "Q" },
+  { keyPress: "W" },
+  { keyPress: "E" },
+  { keyPress: "A" },
+  { keyPress: "S" },
+  { keyPress: "D" },
+  { keyPress: "Z" },
+  { keyPress: "X" },
+  { keyPress: "C" }
+];
+
 function Display(props) {
   return (
     <div id="display">
@@ -8,15 +20,34 @@ function Display(props) {
   );
 }
 
-class App extends React.Component {
+class DrumPad extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
+      <div className="drum-pad">
+        <h1>{this.props.letterKey}</h1>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const drumList = drumPads.map(drumPad => (
+      <DrumPad key={drumPad.keyPress} letterKey={drumPad.keyPress} />
+    ));
+
+    return (
       <div id="drum-machine" className="container bg-primary">
         <Display drumPad="High Hat" />
+        {drumList}
       </div>
     );
   }
