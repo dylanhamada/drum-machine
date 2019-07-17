83acf52cf79fdef83350b8742324c97acea3097d
Dylan Hamada <dylan.hamada@gmail.com>
2019-07-16 21:28:02 -0400

    Updates

--- precache-manifest.2498ea39e7241ca749373b8eae2b771c.js
+++ precache-manifest.2498ea39e7241ca749373b8eae2b771c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69362bd7cf65bef72a18c33378030365",
+    "revision": "3ee461cc108252712bea47deca5991e2",
     "url": "/drum-machine/index.html"
   },
   {
-    "revision": "d5257cd5e3aa32b25588",
-    "url": "/drum-machine/static/css/main.4b2b984a.chunk.css"
+    "revision": "ec1d9854278a03e13b1a",
+    "url": "/drum-machine/static/css/main.90a983f0.chunk.css"
   },
   {
     "revision": "baf961a1263ccd6a2d24",
     "url": "/drum-machine/static/js/2.0f58faaa.chunk.js"
   },
   {
-    "revision": "d5257cd5e3aa32b25588",
-    "url": "/drum-machine/static/js/main.9e1b4e3e.chunk.js"
+    "revision": "ec1d9854278a03e13b1a",
+    "url": "/drum-machine/static/js/main.5811ed74.chunk.js"
   },
   {
     "revision": "afcf9fe73353caf758cf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drum-machine/precache-manifest.9fefb754b286e06aa862fca75964af50.js"
+  "/drum-machine/precache-manifest.2498ea39e7241ca749373b8eae2b771c.js"
 );
 
 self.addEventListener('message', (event) => {
